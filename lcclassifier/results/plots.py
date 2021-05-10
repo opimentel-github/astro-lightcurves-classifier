58ea96f7169e39732b6d137f8e5a80c4579c9a04
opimentel-deepspace <oscarlo.pimentel@gmail.com>
2021-05-10 17:04:53 -0400

    deepspace

--- lcclassifier/results/plots.py
+++ lcclassifier/results/plots.py
@@ -17,7 +17,7 @@ from matplotlib import cm
 
 ###################################################################################################################################################
 
-def plot_metric(rootdir, cfilename, kf, lcset_name, model_names, metric_name,
+def plot_metric(rootdir, cfilename, kf, lcset_name, model_names, dmetrics,
 	baselines_dict={},
 	label_keys=[],
 	figsize=C_.PLOT_FIGZISE_RECT,
@@ -25,69 +25,65 @@ def plot_metric(rootdir, cfilename, kf, lcset_name, model_names, metric_name,
 	p=C_.P_PLOT,
 	alpha=0.2,
 	):
-	fig, axs = plt.subplots(1, 2, figsize=figsize)
-	color_dict = utils.get_color_dict(model_names)
+	for metric_name in dmetrics.keys():
+		fig, axs = plt.subplots(1, 2, figsize=figsize)
+		color_dict = utils.get_color_dict(model_names)
 
-	for kmn,model_name in enumerate(model_names):
-		load_roodir = f'{rootdir}/{model_name}/{train_mode}/performance/{cfilename}/{kf}@{lcset_name}'
-		files, files_ids = fcfiles.gather_files_by_id(load_roodir, fext='d')
-		print(f'ids={files_ids}(n={len(files_ids)}#) - model={model_name}')
-		if len(files)==0:
-			continue
+		for kmn,model_name in enumerate(model_names):
+			load_roodir = f'{rootdir}/{model_name}/{train_mode}/performance/{cfilename}/{kf}@{lcset_name}'
+			files, files_ids = fcfiles.gather_files_by_id(load_roodir, fext='d')
+			print(f'ids={files_ids}(n={len(files_ids)}#) - model={model_name}')
+			if len(files)==0:
+				continue
 
-		survey = files[0]()['survey']
-		band_names = files[0]()['band_names']
-		class_names = files[0]()['class_names']
-		mn_dict = strings.get_dict_from_string(model_name)
-		rsc = mn_dict['rsc']
-		mdl = mn_dict['mdl']
-		is_parallel = 'Parallel' in mdl
-		days = files[0]()['days']
+			survey = files[0]()['survey']
+			band_names = files[0]()['band_names']
+			class_names = files[0]()['class_names']
+			mn_dict = strings.get_dict_from_string(model_name)
+			rsc = mn_dict['rsc']
+			mdl = mn_dict['mdl']
+			is_parallel = 'Parallel' in mdl
+			days = files[0]()['days']
 
-		metric_curve = np.concatenate([f()['days_class_metrics_df'][metric_name].values[None] for f in files], axis=0)
-		#metric_curve = np.concatenate([f()['days_class_metrics_cdf']['SNII-b-n'][metric_name].values[None] for f in files], axis=0) # SNIa SNIbc
-		xe_metric_curve = XError(metric_curve)
-		xe_metric_curve_avg = XError(np.mean(metric_curve, axis=-1))
+			metric_curve = np.concatenate([f()['days_class_metrics_df'][metric_name].values[None] for f in files], axis=0)
+			#metric_curve = np.concatenate([f()['days_class_metrics_cdf']['SNII-b-n'][metric_name].values[None] for f in files], axis=0) # SNIa SNIbc
+			xe_metric_curve = XError(metric_curve)
+			xe_metric_curve_avg = XError(np.mean(metric_curve, axis=-1))
 
-		ax = axs[int(not is_parallel)]
-		_label = strings.get_string_from_dict({k:mn_dict[k] for k in mn_dict.keys() if k in label_keys}, key_key_separator=' - ')
-		label = f'{mdl} ({_label}) | {xe_metric_curve_avg}*'
-		color = color_dict[utils.get_cmodel_name(model_name)]# if rsc=='0' else 'k'
-		ax.plot(days, xe_metric_curve.median, '--' if is_parallel else '-', label=label, c=color)
-		ax.fill_between(days, xe_metric_curve.get_percentile(p), xe_metric_curve.get_percentile(100-p), alpha=alpha, fc=color)
+			ax = axs[int(not is_parallel)]
+			_label = strings.get_string_from_dict({k:mn_dict[k] for k in mn_dict.keys() if k in label_keys}, key_key_separator=' - ')
+			label = f'{mdl} ({_label}) | {xe_metric_curve_avg}*'
+			color = color_dict[utils.get_cmodel_name(model_name)]# if rsc=='0' else 'k'
+			ax.plot(days, xe_metric_curve.median, '--' if is_parallel else '-', label=label, c=color)
+			ax.fill_between(days, xe_metric_curve.get_percentile(p), xe_metric_curve.get_percentile(100-p), alpha=alpha, fc=color)
 
-	title = ''
-	title += f'{metric_name} v/s days'+'\n'
-	title += f'train-mode={train_mode} - survey={survey}-{"".join(band_names)} [{kf}@{lcset_name}]'+'\n'
-	fig.suptitle(title[:-1], va='bottom')
+		mn = metric_name if dmetrics[metric_name]['mn'] is None else dmetrics[metric_name]['mn']
+		title = ''
+		title += f'{mn} v/s days'+'\n'
+		title += f'train-mode={train_mode} - survey={survey}-{"".join(band_names)} [{kf}@{lcset_name}]'+'\n'
+		fig.suptitle(title[:-1], va='bottom')
 
-	for kax,ax in enumerate(axs):
-		is_accuracy = 'accuracy' in metric_name
-		random_guess = 100./len(class_names)
-		if is_accuracy:
-			#ax.plot(days, np.full_like(days, random_guess), ':', c='k', label=f'RandomGuess ({random_guess:.3f}%)', alpha=.5)
-			pass
+		for kax,ax in enumerate(axs):
+			if f'{kf}@{lcset_name}' in baselines_dict.keys():
+				ax.plot(days, np.full_like(days, baselines_dict[f'{kf}@{lcset_name}'][metric_name]), ':', c='k', label=f'FATS & b-RF Baseline (day={days[-1]:.3f})')
 
-		if f'{kf}@{lcset_name}' in baselines_dict.keys():
-			ax.plot(days, np.full_like(days, baselines_dict[f'{kf}@{lcset_name}'][metric_name]), ':', c='k', label=f'FATS & b-RF Baseline (day={days[-1]:.3f})')
+			ax.set_xlabel('time [days]')
+			if kax==1:
+				#ax.set_ylabel(None)
+				#ax.set_yticklabels([])
+				ax.set_title('serial models')
+			else:
+				ax.set_ylabel(mn)
+				ax.set_title('parallel models')
 
-		ax.set_xlabel('time [days]')
-		if kax==1:
-			#ax.set_ylabel(None)
-			#ax.set_yticklabels([])
-			ax.set_title('serial models')
-		else:
-			ax.set_ylabel(metric_name)
-			ax.set_title('parallel models')
+			ax.set_xlim([days.min(), days.max()])
+			#ax.set_xlim([days.min(), days.max()])
+			#ax.set_ylim([random_guess*.95, 90] if is_accuracy else [0, 1])
+			ax.grid(alpha=0.5)
+			ax.legend(loc='lower right')
 
-		ax.set_xlim([days.min(), days.max()])
-		#ax.set_xlim([days.min(), days.max()])
-		#ax.set_ylim([random_guess*.95, 90] if is_accuracy else [0, 1])
-		ax.grid(alpha=0.5)
-		ax.legend(loc='lower right')
-
-	fig.tight_layout()
-	plt.show()
+		fig.tight_layout()
+		plt.show()
 
 ###################################################################################################################################################
 
