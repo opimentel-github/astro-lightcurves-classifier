7c55bfe16cb3dcc1def631f8602ee37bf6d52f6a
opimentel-deepspace <oscarlo.pimentel@gmail.com>
2021-05-10 14:40:55 -0400

    deepspace

--- experiments/master_run.sh
+++ experiments/master_run.sh
@@ -2,10 +2,10 @@
 mids='1000-1005'
 
 txt=""
-txt+="master_run_kfs.sh parallel_attn_models 0 $mids &"
-#txt+="master_run_kfs.sh parallel_rnn_models 1 $mids &"
-#txt+="master_run_kfs.sh serial_attn_models 2 $mids &"
-#txt+="master_run_kfs.sh serial_rnn_models 3 $mids &"
+#txt+="bash master_run_kfs.sh parallel_attn_models 0 $mids &"
+txt+="bash master_run_kfs.sh parallel_rnn_models 0 $mids &"
+#txt+="bash master_run_kfs.sh serial_attn_models 2 $mids &"
+#txt+="bash master_run_kfs.sh serial_rnn_models 3 $mids &"
 
 #echo "$txt"
 eval "$txt"
--- experiments/master_run_kfs.sh
+++ experiments/master_run_kfs.sh
@@ -4,5 +4,7 @@ gpu=$2
 mids=$3
 for kf in 0 1 2 3 4
 do
-eval "nohup python train_deep_models.py -mc $mc -gpu $gpu -mids $mids -kf $kf > /dev/null"
+cmd="nohup python train_deep_models.py -mc $mc -gpu $gpu -mids $mids -kf $kf > /dev/null"
+echo "eval $cmd"
+eval cmd
 done
