825e6d2ea87fd6e5718ebc923369f9015d984dd7
opimentel-deepspace <oscarlo.pimentel@gmail.com>
2021-05-21 17:38:52 -0400

    gru-d

--- lcclassifier/results/tables.py
+++ lcclassifier/results/tables.py
@@ -13,7 +13,7 @@ from scipy.interpolate import interp1d
 
 ###################################################################################################################################################
 
-def get_parallel_serial_df(rootdir, cfilename, kf, lcset_name, model_names, dmetrics,
+def get_ps_performance_df(rootdir, cfilename, kf, lcset_name, model_names, dmetrics,
 	day=None,
 	train_mode='fine-tuning',
 	n=1e3,
@@ -48,6 +48,41 @@ def get_parallel_serial_df(rootdir, cfilename, kf, lcset_name, model_names, dmet
 
 			mn = metric_name if dmetrics[metric_name]['mn'] is None else dmetrics[metric_name]['mn']
 			d[mn] = xe_metric_curve_avg if uses_avg else xe_metric_curve
+
+		index = f'model={utils.get_fmodel_name(model_name)}'
+		info_df.append(index, d)
+
+	return info_df
+
+def get_ps_times_df(rootdir, cfilename, kf, method, model_names,
+	train_mode='pre-training',
+	):
+	info_df = DFBuilder()
+	for kmn,model_name in enumerate(utils.get_sorted_model_names(model_names)):
+		load_roodir = f'{rootdir}/{model_name}/{train_mode}/model_info/{cfilename}'
+		files, files_ids = fcfiles.gather_files_by_kfold(load_roodir, kf, f'train.{method}', fext='d')
+		print(f'ids={files_ids}(n={len(files_ids)}#) - model={model_name}')
+		if len(files)==0:
+			continue
+
+		survey = files[0]()['survey']
+		band_names = files[0]()['band_names']
+		class_names = files[0]()['class_names']
+		is_parallel = 'Parallel' in model_name
+
+		print(files[0]()['monitors']['wmse-xentropy'].keys())
+
+		d = {}
+		parameters = [f()['parameters'] for f in files][0]
+		d['parameters'] = parameters
+		d['best_epoch'] = XError([f()['monitors']['wmse-xentropy']['best_epoch'] for f in files])
+		d['time_per_iteration'] = sum([f()['monitors']['wmse-xentropy']['time_per_iteration'] for f in files])
+		x = files[0]()['monitors']['wmse-xentropy']['time_per_iteration']
+		x = XError(x.x)
+		print(x)
+		d['time_per_iteration/params'] = sum([f()['monitors']['wmse-xentropy']['time_per_iteration']/parameters for f in files])
+		d['time_per_epoch'] = sum([f()['monitors']['wmse-xentropy']['time_per_epoch'] for f in files])
+		d['total_time'] = XError([f()['monitors']['wmse-xentropy']['total_time'] for f in files])
 
 		index = f'model={utils.get_fmodel_name(model_name)}'
 		info_df.append(index, d)
